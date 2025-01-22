2a666b65887e8479890615b8ea456c931063856a
ahmed <ahmed.elmohanned@gmail.com>
2025-01-22 18:47:15 +0200

    added header component and styling for all comps

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,10 @@
-function Header() {
-  return <>header</>;
+function Header({ score, highScore }) {
+  return (
+    <div className="header-component">
+      <h3>Score: {score}</h3>
+      <h3>High-Score: {highScore}</h3>
+    </div>
+  );
 }
 
 export default Header;
