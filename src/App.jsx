ea1536f1b1828962c2aaabc0f193ac82a75418c6
ahmed <ahmed.elmohanned@gmail.com>
2025-01-22 18:46:42 +0200

    refactored to include new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from "react";
 import "./App.css";
 import Header from "./components/header";
 import Cards from "./components/cards";
+import { useState } from "react";
 
 function App() {
+
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
+  if (score > highScore) {
+    setHighScore(score);
+  }
+
+  function handleScore() {
+    setScore(score + 1); 
+  }
+
+  function resetScore() {
+    setScore(0);
+  }
+
   return (
     <>
-      <Header></Header>
-      <Cards></Cards>
+      <Header score={score} highScore={highScore}></Header>
+      <Cards handleScoreIncrease={handleScore} handleScoreReset={resetScore}></Cards>
     </>
   );
 }
